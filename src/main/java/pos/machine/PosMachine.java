b829909f19997d3dd927acc4d2078b2bb63a52c3
Daphne Balane <balanedaphne20@gmail.com>
2023-08-15 21:01:33 +0800

    feat: added decodeToItems function

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,8 +1,21 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class PosMachine {
+
+    private final Map<String, Item> itemMap;
+
+    public PosMachine() {
+        this.itemMap = decodeToItems(ItemsLoader.loadAllItems());
+    }
+
+    private Map<String, Item> decodeToItems(List<Item> items) {
+        return items.stream()
+                .collect(Collectors.toMap(Item::getBarcode, item -> item));
+    }
     public String printReceipt(List<String> barcodes) {
         return null;
     }
